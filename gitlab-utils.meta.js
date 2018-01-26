3000ab9b16c7addd22066d2ff72dc832373f22bc
Ilya Gulya <ilyagulya@gmail.com>
2018-01-26 15:26:09 +0700

    Create gitlab-utils.meta.js

---
+++ gitlab-utils.meta.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         Gitlab useful addons
+// @namespace    http://gitlab.ccrowd.in/
+// @version      0.0.5
+// @description  some useful things for gitlab
+// @author       Ilya Gulya
+// @match        https://git.i20.biz/*
+// @grant        none
+// ==/UserScript==
