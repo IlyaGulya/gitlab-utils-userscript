f7b33a0892e966e2f96e265f83d7f6fdb3f9b270
Ilya Gulya <ilyagulya@gmail.com>
2018-01-26 15:27:54 +0700

    Update gitlab-utils.user.js

--- gitlab-utils.user.js
+++ gitlab-utils.user.js
@@ -6,6 +6,8 @@
 // @author       Ilya Gulya
 // @match        https://git.i20.biz/*
 // @grant        none
+// @downloadURL  https://github.com/IlyaGulya/gitlab-utils-userscript/raw/master/gitlab-utils.user.js
+// @updateURL    https://github.com/IlyaGulya/gitlab-utils-userscript/raw/master/gitlab-utils.meta.js
 // ==/UserScript==
 
 function join(arr, char) {
