2b0af2583a733f440e63d9949f8e38349a4ea936
Ilya Gulya <gulya.i@devicement.com>
2018-02-08 18:48:43 +0700

    Add ability to linkify titles to Redmine tasks

--- gitlab-utils.user.js
+++ gitlab-utils.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Gitlab useful addons
 // @namespace    http://gitlab.ccrowd.in/
-// @version      0.0.5
+// @version      0.0.8
 // @description  some useful things for gitlab
 // @author       Ilya Gulya
 // @match        https://git.i20.biz/*
@@ -12,7 +12,7 @@
 
 function join(arr, char) {
     var str = "";
-    for (var i = 0; i< arr.length; i++) {
+    for (var i = 0; i < arr.length; i++) {
         str += arr[i];
         if (i > 0 && i < arr.length - 1) {
             str += char;
@@ -31,21 +31,53 @@ function addFromAnnotations(titles) {
     }
 }
 
-$(function() {
-    'use strict';
-    var titleField = $("#merge_request_title");
-    var titles = $("a.commit-row-message.item-title").map((n, item) => {
+function collectMRTitlesFromCommitMessages() {
+    const titleField = $("#merge_request_title");
+    const titles = $("a.commit-row-message.item-title").map((n, item) => {
         return $(item).text()
-                      .split("\n")
-                      .filter(str => str.length > 0)[0];
+            .split("\n")
+            .filter(str => str.length > 0)[0];
     });
-    titles.splice(0,0, "Append title from commit");
-    var select = $(`<select id="append_title_from_last_commit">${join(titles.map((i, str) => `<option>${str}</option>`), "")}</select>`);
+    titles.splice(0, 0, "Append title from commit");
+    const select = $(`<select id="append_title_from_last_commit">${join(titles.map((i, str) => `<option>${str}</option>`), "")}</select>`);
     select.insertAfter(titleField);
     select.change(() => {
-        var title = select.val();
+        const title = select.val();
         if (title != "Append title from commit") {
-          titleField.val(select.val());
+            titleField.val(select.val());
         }
     });
+}
+
+function replaceTaskIdsWithLinks() {
+    const allTitles = Array.from(document.querySelectorAll(".merge-request h2.title"));
+    allTitles
+        .map(node => {
+            const match = node.innerText.match(/\[#(\d+)\]/);
+            return {
+                node: node,
+                match: match
+            };
+        })
+        .filter(data => data.match.length >= 2)
+        .map(data => {
+            return Object.assign({}, data, {
+                matchFull: data.match[0],
+                matchDigits: data.match[1]
+            });
+        })
+        .forEach(data => {
+            const link = $(`<a href="https://pm.i20.biz/issues/${data.matchDigits}">${data.matchFull}</a>`);
+            data.node.innerText.replace(data.matchFull, "");
+            const titleText = data.node.innerText.replace(data.matchFull, "");
+            const titleTextElement = $(`<span>${titleText}</span>`);
+            data.node.appendChild(link);
+            data.node.appendChild(titleText);
+        })
+}
+
+$(function () {
+    'use strict';
+    collectMRTitlesFromCommitMessages();
+    replaceTaskIdsWithLinks();
 });
